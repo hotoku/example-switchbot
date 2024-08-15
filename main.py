97f66567c31469495ceaca84ea7c89469f4c71d2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-08-16 06:45:43 +0900

    [upd] show devices

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import json
-import time
+from pprint import pprint
+import base64
 import hashlib
 import hmac
-import base64
+import json
+import time
 import uuid
+
+import certifi
+import requests
+import urllib3
 
 # Declare empty header dictionary
 apiHeader = {}
@@ -18,18 +23,25 @@ string_to_sign = '{}{}{}'.format(token, t, nonce)
 string_to_sign = bytes(string_to_sign, 'utf-8')
 secret = bytes(secret, 'utf-8')
 
-sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
-print ('Authorization: {}'.format(token))
-print ('t: {}'.format(t))
-print ('sign: {}'.format(str(sign, 'utf-8')))
-print ('nonce: {}'.format(nonce))
+sign = base64.b64encode(
+    hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
 
-#Build api header JSON
-apiHeader['Authorization']=token
-apiHeader['Content-Type']='application/json'
-apiHeader['charset']='utf8'
-apiHeader['t']=str(t)
-apiHeader['sign']=str(sign, 'utf-8')
-apiHeader['nonce']=str(nonce)
+# Build api header JSON
+apiHeader['Authorization'] = token
+apiHeader['Content-Type'] = 'application/json'
+apiHeader['charset'] = 'utf8'
+apiHeader['t'] = str(t)
+apiHeader['sign'] = str(sign, 'utf-8')
+apiHeader['nonce'] = str(nonce)
 
-print(apiHeader)
+
+http = urllib3.PoolManager(
+    cert_reqs="CERT_REQUIRED",
+    ca_certs=certifi.where()
+)
+
+host = "https://api.switch-bot.com"
+ret = http.request("get", url=host + "/v1.0/devices", headers=apiHeader)
+val = ret.json()
+
+pprint(val)
